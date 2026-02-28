da7c8a031150a18cf3287fdd5b3958570e360d18
paramSonawane <paramsonawane@gmail.com>
2026-02-28 21:27:03 +0530

    miryoku-mouse: move layers config to separate folder

---
+++ config/miryoku_behaviors/layers/constants.h
@@ -0,0 +1,7 @@
+#define U_BASE 0
+#define U_NUM 1
+#define U_SYM 2
+#define U_NAV 3
+#define U_FUN 4
+#define U_MOUSE 6
+#define U_BUTTON 8
---
+++ config/miryoku_behaviors/layers/layers.h
@@ -0,0 +1,9 @@
+#include "constants.h"
+
+MIRYOKU_X(BASE, "Base")
+MIRYOKU_X(NUM, "Num")
+MIRYOKU_X(SYM, "Sym")
+MIRYOKU_X(NAV, "Nav")
+MIRYOKU_X(FUN, "Fun")
+MIRYOKU_X(MOUSE, "Mouse")
+MIRYOKU_X(BUTTON, "Button")

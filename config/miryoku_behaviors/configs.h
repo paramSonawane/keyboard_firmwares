ae927a3c56e21a2ad651c4b0b1d26a01aff0897b
paramSonawane <paramsonawane@gmail.com>
2026-02-28 21:15:06 +0530

    miryoku-mouse: move mouse functions to separate folder

--- config/miryoku_behaviors/configs.h
+++ config/miryoku_behaviors/configs.h
@@ -1,3 +1,1 @@
 #define U_TAPPING_TERM 200
-
-#define M_SENS 1250
--- config/miryoku_behaviors/mouse/mouse.h
+++ config/miryoku_behaviors/mouse/mouse.h
@@ -1,24 +1,29 @@
-// Copyright 2022 Manna Harbour
-// https://github.com/manna-harbour/miryoku
+#include "constants.h"
 
-#pragma once
+&mmv {
+  acceleration-exponent = <U_MOUSE_MOVE_EXPONENT>;
+  time-to-max-speed-ms = <U_MOUSE_MOVE_TIME>;
+  delay-ms = <U_MOUSE_MOVE_DELAY>;
+};
 
-#define U_MOUSE_MOVE_EXPONENT 1
-#define U_MOUSE_MOVE_TIME 1500
-#define U_MOUSE_MOVE_DELAY 0
-#define U_MOUSE_SCROLL_EXPONENT 1
-#define U_MOUSE_SCROLL_TIME 5000
-#define U_MOUSE_SCROLL_DELAY 0
+&msc {
+  acceleration-exponent = <U_MOUSE_SCROLL_EXPONENT>;
+  time-to-max-speed-ms = <U_MOUSE_SCROLL_TIME>;
+  delay-ms = <U_MOUSE_SCROLL_DELAY>;
+};
 
 #define U_BTN1 &mkp MB1
 #define U_BTN2 &mkp MB2
 #define U_BTN3 &mkp MB3
 #define U_BTN4 &mkp MB4
 #define U_BTN5 &mkp MB5
+
+
 #define U_MS_D &mmv MOVE_DOWN
 #define U_MS_L &mmv MOVE_LEFT
 #define U_MS_R &mmv MOVE_RIGHT
 #define U_MS_U &mmv MOVE_UP
+
 #define U_WH_D &msc SCRL_DOWN
 #define U_WH_L &msc SCRL_LEFT
 #define U_WH_R &msc SCRL_RIGHT
